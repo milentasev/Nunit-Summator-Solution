c6675096b998aec13814424d94f979657a608f0b
milentasev <48089006+milentasev@users.noreply.github.com>
2022-05-07 15:39:28 +0300

    Add project

---
+++ Summator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Summator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Summator summator = new Summator();
+
+			long result = summator.Sum(new int[] { 1, 2, 3, 4, 5, 6, });
+			if (result == 21) {
+				Console.WriteLine("TEST PASS");
+			}
+			else {
+				Console.WriteLine("TEST FAIL");
+			}
+
+			double average = summator.Avarage(new double[] {5, 10, 15});
+			
+			if (average == 10)
+			{
+				Console.WriteLine("TEST PASS");
+			}
+			else
+			{
+				Console.WriteLine("TEST FAIL");
+			}
+		}
+	}
+}
---
+++ Summator/Summator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summator
+{
+	public  class Summator
+	{
+		public long Sum(int[] arr)
+		{
+
+			long sum = 0;
+
+				for (int i = 0; i < arr.Length; i++)
+				{
+					sum += arr[i];
+				}
+
+				return sum;
+
+		}
+
+		public double Avarage(double[] arr)
+		{
+
+			double sum = 0;
+
+			for (int i = 0; i < arr.Length; i++)
+			{
+				sum += arr[i];
+			}
+
+			return sum/arr.Length;
+
+		}
+	}
+}
